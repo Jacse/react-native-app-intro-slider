c9bf1e12e965a2b86e4f4579ec7b7725b9834b3b
Jacob Lauritzen <jacsebl@hotmail.com>
2018-11-09 13:01:59 -0500

    Update example to newest expo and react native

--- Example/App.js
+++ Example/App.js
@@ -26,29 +26,31 @@ const styles = StyleSheet.create({
     backgroundColor: 'transparent',
     textAlign: 'center',
     marginBottom: 16,
-  }
+  },
 });
 
 const slides = [
   {
     key: 'somethun',
     title: 'Quick setup, good defaults',
-    text: 'React-native-app-intro-slider is easy to setup with a small footprint and no dependencies. And it comes with good default layouts!',
-    icon: 'ios-images-outline',
+    text:
+      'React-native-app-intro-slider is easy to setup with a small footprint and no dependencies. And it comes with good default layouts!',
+    icon: 'ios-images',
     colors: ['#63E2FF', '#B066FE'],
   },
   {
     key: 'somethun1',
     title: 'Super customizable',
-    text: 'The component is also super customizable, so you can adapt it to cover your needs and wants.',
-    icon: 'ios-options-outline',
+    text:
+      'The component is also super customizable, so you can adapt it to cover your needs and wants.',
+    icon: 'ios-options',
     colors: ['#A3A1FF', '#3A3897'],
   },
   {
     key: 'somethun2',
     title: 'No need to buy me beer',
     text: 'Usage is all free',
-    icon: 'ios-beer-outline',
+    icon: 'ios-beer',
     colors: ['#29ABE2', '#4F00BC'],
   },
 ];
@@ -56,16 +58,25 @@ const slides = [
 export default class App extends React.Component {
   _renderItem = props => (
     <LinearGradient
-      style={[styles.mainContent, {
-        paddingTop: props.topSpacer,
-        paddingBottom: props.bottomSpacer,
-        width: props.width,
-        height: props.height,
-      }]}
+      style={[
+        styles.mainContent,
+        {
+          paddingTop: props.topSpacer,
+          paddingBottom: props.bottomSpacer,
+          width: props.width,
+          height: props.height,
+        },
+      ]}
       colors={props.colors}
-      start={{x: 0, y: .1}} end={{x: .1, y: 1}}
+      start={{ x: 0, y: 0.1 }}
+      end={{ x: 0.1, y: 1 }}
     >
-      <Ionicons style={{ backgroundColor: 'transparent' }} name={props.icon} size={200} color="white" />
+      <Ionicons
+        style={{ backgroundColor: 'transparent' }}
+        name={props.icon}
+        size={200}
+        color="white"
+      />
       <View>
         <Text style={styles.title}>{props.title}</Text>
         <Text style={styles.text}>{props.text}</Text>
